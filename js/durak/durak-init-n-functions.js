4a7b47105f35b8ccebd18e7a644579976f0eb819
Michel van Gruijthuijsen <mistige@gmail.com>
2019-07-28 11:26:05 +0200

    add primers for class Card, func buildDeck

--- js/durak/durak-init-n-functions.js
+++ js/durak/durak-init-n-functions.js
@@ -1,5 +1,6 @@
 // === durak-init-n-functions ===
     // --- init ---
+    var deck = [];
     var house=["c1","c2","c3","c4","c5","c6","c7","c8","c9","c10","c11","c12","c13","c14","c15","c16","c17","c18","c19","c20","c21","c22","c23","c24","c25","c26","c27","c28","c29","c30","c31","c32","c33","c34","c35","c36","c37","c38","c39","c40","c41","c42","c43","c44","c45","c46","c47","c48","c49","c50","c51","c52","c53","c54"];
     //var house=["c1","c2","c3","c4"];
     var yourhand=[];
@@ -16,6 +17,25 @@
     var igorsays="";
     // ---
     var lastCard="-";
+
+
+    // --- classes ---
+    class Card {
+	constructor(cid, altName) {
+	    this.cid = cid;
+	    this.altName = altName;
+	}
+    }
+
+
+    // --- build deck ---
+    function buildDeck() {
+	var i = 0;
+        for ( ; i< deck.length ; i++ ) {
+            deck[i].cid = i;
+	    deck[i].altName = descCard(deck[i]);
+	}
+    }
 
 
     // --- functions ---
@@ -61,7 +81,6 @@
         var housestring="";
         buildhouse: {
             var i, row;
-
             var rowmax=6;
             var columnmax=9;
             for (row=0; row < rowmax ; row++ ) {

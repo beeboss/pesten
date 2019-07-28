0b64cca00a349e76fddcc42f6f596b4ee28c8f2b
Michel van Gruijthuijsen <mistige@gmail.com>
2019-07-28 19:17:59 +0200

    1/2: rename durak-> pesten ; 2/2: add background image

---
+++ js/pesten/old/counter.js
@@ -0,0 +1,11 @@
+//;function stack-init() {
+//    var cardstack=[
+
+// conter om ff de getallen voor me op te noemen ipv uittypen. oefening in js en in emacs
+
+function counter() {
+    var i;
+    for (i = 1 ; i < 54 ; i++) {
+	console.log(i);
+	}
+}

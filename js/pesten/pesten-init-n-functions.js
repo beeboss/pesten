a70054ec2fe06e88acceba6610ef687b625f1602
Michel van Gruijthuijsen <mistige@gmail.com>
2019-07-31 09:08:05 +0200

    move card-as-object primer into its own project

--- js/pesten/pesten-init-n-functions.js
+++ js/pesten/pesten-init-n-functions.js
@@ -19,26 +19,6 @@
     var lastCard="-";
 
 
-    // --- classes ---
-    class Card {
-	constructor(cid, altName) {
-	    this.cid = cid;
-	    this.altName = altName;
-	}
-    }
-
-
-    // --- build deck ---
-    function buildDeck() {
-	var i = 0;
-        for ( ; i < house.length ; i++ ) {
-            deck[i] = new Card;
-	    deck[i] = { cid: "c" + i, altName: descCard(deck[i]) };
-	    //  deck[i].altName = descCard(deck[i]);
-	}
-    }
-
-
     // --- functions ---
     function shuffle(array) {
         var currentIndex = array.length, temporaryValue, randomIndex;

967918743c6b2a09d2ffe50a0b8d177964661267
Michel van Gruijthuijsen <mistige@gmail.com>
2019-08-04 19:32:06 +0200

    fix shuffle button and other stuff (= fix error in missing css)

--- js/pesten/pesten-init-n-functions.js
+++ js/pesten/pesten-init-n-functions.js
@@ -2,7 +2,7 @@
     // --- init ---
     var deck = [];
     var house=["c1","c2","c3","c4","c5","c6","c7","c8","c9","c10","c11","c12","c13","c14","c15","c16","c17","c18","c19","c20","c21","c22","c23","c24","c25","c26","c27","c28","c29","c30","c31","c32","c33","c34","c35","c36","c37","c38","c39","c40","c41","c42","c43","c44","c45","c46","c47","c48","c49","c50","c51","c52","c53","c54"];
-    var house=["c1","c2"];
+    // var house=["c1","c2"];
     var yourhand=[];
     var ighand=[];
     var curhand=[];
@@ -75,7 +75,7 @@
             }
 
         }
-        document.getElementById("id-yourhand").innerHTML=testString;
+        document.getElementById("id-house").innerHTML=housestring;
     }
 
 

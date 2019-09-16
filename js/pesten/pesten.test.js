1dfa48b4830d7707eacf9baacc0f55180f190dd7
Michel van Gruijthuijsen <mistige@gmail.com>
2019-09-16 16:42:51 +0200

    add pesten.test.js

---
+++ js/pesten/pesten.test.js
@@ -0,0 +1,22 @@
+const { switchUser, descCard } = require('./pesten');
+
+// -----------
+test('should output "igor" or "you"',  () => {
+    const text = switchUser("you");
+    expect(text).toBe("igor");
+
+    const text2 = switchUser("igor");
+    expect(text2).toBe("you");
+    });
+
+
+// -----------
+test('should output [schoppen aas]', () => {
+    const card1 = descCard('c1');
+    expect(card1).toBe("schoppen aas");
+});
+
+test('should output [joker]', () => {
+    const card1 = descCard('c53');
+    expect(card1).toBe("joker");
+});

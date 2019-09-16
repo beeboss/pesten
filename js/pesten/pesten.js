6d2dc572e88c8f57e7ad383380ea59461386a915
Michel van Gruijthuijsen <mistige@gmail.com>
2019-09-16 17:38:16 +0200

    big number of changes, see extra description
           added possible future funcs
           fixed joker being named "joker aas" by descCard
           some cleanup

--- js/pesten/pesten.js
+++ js/pesten/pesten.js
@@ -2,8 +2,12 @@
     // --- init ---
     var card = [];
     var deck = [];
+    // @@TODO: array voor house bouwen: for/while loop mk, de 'c' ervoor plakken met een map ofzo
     var house=["c1","c2","c3","c4","c5","c6","c7","c8","c9","c10","c11","c12","c13","c14","c15","c16","c17","c18","c19","c20","c21","c22","c23","c24","c25","c26","c27","c28","c29","c30","c31","c32","c33","c34","c35","c36","c37","c38","c39","c40","c41","c42","c43","c44","c45","c46","c47","c48","c49","c50","c51","c52","c53","c54"];
     // var house=["c1","c2"];
+    var cardNamesNL=["aas","2","3","4","5","6","7","8","9","10","boer","vrouw","heer"];
+    var cardNamesRU=["туз","2","3","4","5","6","7","8","9","10","валет","дама","король"];
+    var suitlist=["schoppen","harten","klaveren","ruiten"];
     var yourhand=[];
     var ighand=[];
     var curhand=[];
@@ -21,11 +25,25 @@
     var lastCard="-";
     var testString="<h1>ZZZZZZZZZZZZZZZZZZZ</h1>";
 
+
     // === user-defined vars ===
     defCardWidthPerc = 6;
 
 
     // === functions ===
+
+    function buildDeck() {
+        var i = 0;
+        while ( i < 54 ) {
+            house[i]="c" + i;
+            i++;
+            }
+        }
+
+
+    function cardNames() {
+        }
+
 
     function updScreen() {
         document.getElementById("id-nextup").innerHTML = "nextup: " + nextup ;
@@ -42,13 +60,12 @@
         document.getElementById("id-debug-igor").innerHTML = "igor: " + ighand ;
         document.getElementById("id-debug-you").innerHTML = "you: " + yourhand ;
         // reset values that are not updated with every move
-	    igorsays="";    // reset
+	    igorsays="...";    // reset
     }
 
 
     function shuffle(array) {
         var currentIndex = array.length, temporaryValue, randomIndex;
-
         // While there remain elements to shuffle...
         while (0 !== currentIndex) {
 
@@ -65,13 +82,12 @@
         }
 
 
-    exports.switchUser = (tmpUser) => {
+    function switchUser(tmpUser) {
         if ( tmpUser == "you" ) {
             user = "igor";
             curhand = ighand;
             curHandString = igHandString;
             nextup = "you";
-            actionIgor();
             return user;
             }
         else {
@@ -79,7 +95,6 @@
             curhand = yourhand;
             curHandString = yourHandString;
             nextup = "igor";
-            actionYou();
             return user;
             }
         }
@@ -95,26 +110,27 @@
     }
 
 
-    function actionIgor() {
-        // strategie igor: gooi "eerste-de-beste" kaart neer als dat volgens regels mag, heb je niks, koop kaart
-        var i = 0;
-        while (  i < ighand[(ighand.length-1)] ) {
-            if ( ckCardPlayable[i] == 1 ) {
-                igorsays="I throw ighand[i]";
-                buildHandString();
-                playCard(ighand[i]);
-                i++;
+    function actionPlayer(tmpUser) {
+        if ( tmpUser == "igor") {
+            // strategie igor: gooi "eerste-de-beste" kaart neer als dat volgens regels mag, heb je niks, koop kaart
+            var i = 0;
+            while (  i < ighand[(ighand.length-1)] ) {
+                if ( ckCardPlayable[i] == 1 ) {
+                    igorsays="I throw ighand[i]";
+                    playCard(ighand[i]);
+                    i++;
+                    }
                 }
+                bCard = buyTopCard();
+                igorsays="I buy a card ... :(";
+                curhand.push(bCard);
             }
-            bCard = buyCard();
-            igorsays="I buy a card ... :(";
-            curhand.push(bCard);
+        else     // user == you
+           // human *chooses* , either to drop a card, or to buy a card
+           youTryHandCard();
         }
 
 
-    function actionYou() {
-        // human *chooses* , either to drop a card, or to buy a card
-        youTryHandCard();
 
 
     function youTryHandCard() {
@@ -124,13 +140,14 @@
          mayBePlayed = ckCardPlayable(e.target.id);
          if ( mayBePlayed == 0 ) {
              comment = "not a valid card";
+             return false;
              }
          else {
              yourhand.splice(yourhand.indexOf(e.target.id), 1);
-             buildHandString();
              playCard(e.target.id);
+             return true;
              }})
-        }
+         }
 
 
     function playCard(cCard) {
@@ -205,16 +222,33 @@
         curhand.push(cardcontent);
         }
 
+
     function buyTopCard() {
         house.splice(0,1);
         curhand.push(cardcontent);
         }
 
+
+    function desc2(cCard) {
+     	var serial = cCard.replace("c","");
+		var altName="";
+        var suitNumber = Math.floor(serial/13);
+        var suit = suitlist.suitNumber;
+        var value = serial - (suitNumber*13);
+        // igorsays = cardNamesRU.value;
+        }
+
+
     function descCard(cCard) {
      	var serial = cCard.replace("c","");
 		var altName="";
 		var suit="";
-		// -- suit
+        if ( serial == 52 || serial == 53 ) {
+            altName = "joker";
+            last = altName;
+            return altName;
+            }
+		// -- non-joker cards
 		if (serial <= 13) { suit = "schoppen" ; }
 		else if (serial <= 26) { suit = "harten" ; }
 		else if (serial <= 39) { suit = "klaver" ; }
@@ -241,19 +275,22 @@
 			   break;
 			default:
 			   value = rad ;
-			   igorsays="...";
       	       }
-	        altName = suit + " " + value;
-	        last = altName;
-            return altName;
-            }
-
+	    altName = suit + " " + value;
+	    last = altName;
+        return altName;
+        }
 
 
     function takeSequence() {
         switchUser(user);
-        // @@TODO: player kan OFWEL card on table gooien OFWEL buyCard();
-	    descCard(curhand[(curhand.length-1)]);
+        actionPlayer(user);
         refreshHand();
         buildHouseString();
         }
+
+
+    // ---------------
+    // --- exports ---
+    exports.switchUser = switchUser;
+    exports.descCard = descCard;

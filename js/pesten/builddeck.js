b6dffc865c6381fcdd088e5d37421830b12b2db9
Michel van Gruijthuijsen <mistige@gmail.com>
2019-10-08 14:53:10 +0200

    pesten.test.js: add a test that should always succeed

---
+++ js/pesten/builddeck.js
@@ -0,0 +1,10 @@
+var kaartspel[];
+
+function buildDeck() {
+    var i = 0;
+    while (i < 54) {
+        kaartspel[i]="c"+i;
+    }
+}
+
+console.log(kaartspel);
--- js/pesten/pesten.test.js
+++ js/pesten/pesten.test.js
@@ -1,18 +1,27 @@
-const { switchUser, descCard , ckCardPlayable, house, user } = require('./pesten');
+// const { switchUser, descCard , ckCardPlayable, house, user } = require('./pesten');
+const { descCard } = require('./pesten');
 
+// ---- test the tester -- no deps on code ----
+test('object assignment', () => {
+  const data = {one: 1};
+  data['two'] = 2;
+  expect(data).toEqual({one: 1, two: 2});
+});
 
 
 // -----------
 test('should output "schoppen aas"', () => {
     const card1 = descCard('c1');
     expect(card1).toBe("schoppen aas");
-    });
+});
+
 
 test('should output "joker"', () => {
     const card1 = descCard('c53');
     expect(card1).toBe("joker");
     });
 
+/* park ff
 //------------
 test('[housecards] should output "c1"', () => {
     const cNumber = house[0];
@@ -28,3 +37,4 @@ test('[igorThink] should output 1 #without quotes', () => {
     const cReturnValueCCP = ckCardPlayable();
     expect(cReturnValueCCP).toBe(1);
 });
+*/

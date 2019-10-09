08f43103160cf4f13ccd88e92c47ab2b08f3fcf8
Michel van Gruijthuijsen <mistige@gmail.com>
2019-10-09 11:35:28 +0200

    waitforhuman: save wip state

--- js/trainen/waitforhuman/waitforhuman.js
+++ js/trainen/waitforhuman/waitforhuman.js
@@ -1,19 +1,20 @@
 // === waitforhuman.js ===
 
-var boolDone = "no";
+// @@TODO: ms kan ik iedere key opvangen en ze als chocolade letters weergeven #sinterklaas
 
 function loopArrayAsync(ar, callback) {
     var index;
 
     index = 0;
-    miepString = "";
+    humanString = "";
     loop();
 
     function loop() {
         if (index < ar.length) {
-            miepString = miepString + (ar[index++]) + " ";
-            setTimeout(loop, 1000);
-            document.getElementById("id_miepstring").innerHTML = miepString;
+            humanString = humanString + (ar[index++]) + " ";
+            // set a timeout to keep cpu cool
+            setTimeout(loop, 33);
+            document.getElementById("id_humanstring").innerHTML = humanString;
         }
         else {
             callback();
@@ -22,12 +23,22 @@ function loopArrayAsync(ar, callback) {
 }
 
 function init() {
+    document.getElementById("id_instruction").innerHTML = "Please type 5 words and end with a '.'";
     document.getElementById("id_jobstatus").innerHTML = "busy..";
 }
 
 
+function getWord() {
+
+    // get input word-by-word
+}
+
+function countWords() {
+    // counts/checks if user actually typed requested word count
+}
+
 function myLoop() {
-    var a = ["Yes", "," , "I", "think", "this", "will", "work", "," , "Mr.", "Watson", "."];
+
     loopArrayAsync(a,
                    function() {
                        document.getElementById("id_jobstatus").innerHTML = "(ok) job done";

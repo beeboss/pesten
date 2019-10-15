e5a790cdcff7cce8371e7e3632288b4969d2e0f7
Michel van Gruijthuijsen <mistige@gmail.com>
2019-10-15 13:43:36 +0200

    createEl: random animals displayed. TODO: get css colors working

--- js/snippets/createEl.js
+++ js/snippets/createEl.js
@@ -1,17 +1,39 @@
 // ---- createEl.js ----
+// @@TODO:OPTIE later ogeven *welk* element je wilt hebben, content vh el voorlopig static (1text,1img,..)
 
-// @@TODO: werkend krijgen
-// @@TODO: later ogeven *welk* element je wilt hebben, content vh el voorlopig static (1text,1img,..)
+
+var arrayAnimals = [
+    "dog","cat","owl","horse","cow","mouse","mosquito","bat","donkey","monkey","bird"
+]
+
 
 function createEl(elType) {
-    var mig = document.createTextNode("mig-was-here");
-///=========
-    if ( elType == "div" ) {
+    if  ( ckAnimalsLeft())  {
         var newDiv = document.createElement("div");
-        var newContent = document.createTextNode("created TextNode");
+        var newContent = document.createTextNode(randomAnimal());
         newDiv.appendChild(newContent);
         var currentDiv = document.getElementById("id_hook");
         document.body.insertBefore(newDiv,currentDiv);
-
     }
+    else {
+       document.getElementById("id_status").innerHTML = "no more animals left";
+    }
+}
+
+
+function ckAnimalsLeft() {
+    if ( arrayAnimals.length == 0 ) {
+        return 0;
+    }
+    else {
+        return 1;
+    }
+}
+
+
+function randomAnimal() {
+    var rNumber = Math.floor(Math.random() * arrayAnimals.length);
+    var rAnimal = arrayAnimals[rNumber];
+    arrayAnimals.splice(rNumber,1);     // remove entry from array
+    return rAnimal;
 }

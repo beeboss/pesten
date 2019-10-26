d3ed87bd360e3a76fab7a53485e01b716e51ddef
Michel van Gruijthuijsen <mistige@gmail.com>
2019-10-26 12:20:42 +0200

    add reloadpg primer, small restructure in dirs

---
+++ js/snippets/reloadpg/reloadpg.js
@@ -0,0 +1,7 @@
+function reloadPg() {
+    while ( 1 == 1 ) {
+        // window.location.replace('https://www.teamtrees.org');
+        setTimeout( function() {} , 3000);
+        // setTimeout(window.location.reload(true),3000);
+    }
+}
